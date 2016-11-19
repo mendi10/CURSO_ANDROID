07ba63b7910ec35e429a8315e0978fdf0d234202
mendi10 <yesmendi@gmail.com>
2016-11-19 10:46:44 -0500

    final check

--- Ligth_app/app/src/main/java/com/example/a62/ligth_app/MainActivity.java
+++ Ligth_app/app/src/main/java/com/example/a62/ligth_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a62.ligth_app;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
@@ -7,6 +8,7 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -20,9 +22,12 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     private static  final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_HANDLE_CAMARA_PERM = 2;
     private Camera camera;
     private boolean isFlash;
-
+    private Camera.Parameters param;
+    private boolean isFlashOn;
+    private  ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         isFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
 
-        if(!isFlash){ //si la cmara no tiene flash, muestra un mensaje y cierra la app
+        if(!isFlash){ //si la ca mara no tiene flash, muestra un mensaje y cierra la app
             Log.e(TAG,"NO HAY FLASH");
             AlertDialog.Builder builder = new  AlertDialog.Builder(this)
                 .setTitle("Alerta")
@@ -50,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.show();
         }
 
-        ToggleButton toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
+        toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
 
         //se crea el listener para el boton
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -72,24 +77,96 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        getCamera();
-        
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED){
+            getCamera();
+        }else {
+            requestCameraPermission();
+        }
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Aplicacion Destruida");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+         camera.release();
+         camera = null;
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(isFlashOn){
+
+            ledOn();
+        }
+    }
+
+    private void requestCameraPermission() {
+
+        final  String[] permissions = new String[] {Manifest.permission.CAMERA};
+
+        ActivityCompat.requestPermissions(this,permissions,RC_HANDLE_CAMARA_PERM);
+
     }
 
     private void getCamera() {
+
+        if (camera == null){
+            camera = Camera.open();
+            param = camera.getParameters();
+        }else{
+            Log.e(TAG,"Error de Camara");
+        }
     }
 
     private void turnOffFlash() {
-        
-        
+
+        if(isFlashOn){
+            if (camera == null || param == null) return;
+            param.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(param);
+            camera.startPreview();
+            isFlashOn = false;
+            Log.i(TAG,"El Flash ha sido Apagado..");
+        }
+
     }
 
     private void turnOnFlash() {
+
+        if(!isFlashOn){
+             ledOn();
+        }
     }
+
+    private void ledOn() {
+
+        if (camera == null || param == null) return;
+        param.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(param);
+        camera.startPreview();
+        isFlashOn = true;
+        Log.i(TAG,"El Flash ha sido encendido");
+    }
+
 
 }
